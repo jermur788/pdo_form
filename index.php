aba050f240ba75d79dcb8bc8e8df7b30cb670ed1
jerry <murphyjerry788@gmail.com>
2020-10-26 15:00:33 +0000

    connected form to datbase and added functionality to create a new user

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!doctype html>
 <html lang="en">
 
@@ -18,20 +19,17 @@
   <div class="container">
     <div class="row justify-content-center">
       <div class="col-">
-        <form class=form-container>
+        <form class=form-container method=post action=new_user.php>
           <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <label for="user_name">Your Name</label>
+            <input type="text" class="form-control" name="user_name" id="user_name" aria-describedby="userName">
+           
           </div>
           <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1">
+            <label for="password">Password</label>
+            <input type="password" class="form-control" name="password" id="password">
           </div>
-          <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-          </div>
+      
           <button type="submit" class="btn btn-primary btn-block">Submit</button>
         </form>
       </div>
